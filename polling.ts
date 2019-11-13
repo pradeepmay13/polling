a3f80113077a06ee6ca2b6943b8ade5c47329ecc
pradeepmay13 <pradeepmay13@gmail.com>
2019-11-13 19:51:23 +0800

    Update polling.ts

--- polling.ts
+++ polling.ts
@@ -1,4 +1,16 @@
-public pollingTask() {
+import { Component, OnInit } from '@angular/core';
+import { timer, from, of, Observable, pipe } from 'rxjs'
+import { map, concatMap, filter, take, mergeMap, merge } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+    constructor(private readonly http: HttpClient) {}
+    public pollingTask() {
     const httpUrl = this.http.get('http://localhost:3004/posts');
     timer(0, 1000)
       .pipe(concatMap(() => httpUrl),
@@ -7,4 +19,5 @@ public pollingTask() {
       .pipe(filter((result: any) => result.status === false))
       .pipe(take(1))
       .subscribe((response) => console.log(response));
- }
+    }
+}
